55656acfc94fcc337311a7acb3fd3946639ab8b0
wickdhdgamer <niru.saba@hotmail.com>
2020-04-03 03:48:58 -0400

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,7 +19,7 @@
 <body>
     <header>
         <nav>
-            <strong>{{ WRONG WEBSITE }}</strong><br>
+            <strong>{{ $page->site->title }}</strong><br>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/posts">Posts</a></li>
