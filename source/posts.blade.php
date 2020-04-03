3a030d7f13c28a606eb5cdda10ff1082755736b5
wickdhdgamer <niru.saba@hotmail.com>
2020-04-03 03:42:24 -0400

    Update posts.blade.php

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -5,11 +5,6 @@
 @section('content')
     <h1>Posts</h1>
 
-    <ul>
-        @forelse ($posts->sortBy('title') as $post)
-            <li>
-                <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
-                <small>{{ date('M j, Y', $post->date) }}</small>
             </li>
         @empty
             <p>No posts to show.</p>
